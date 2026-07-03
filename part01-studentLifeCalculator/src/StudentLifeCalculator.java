94c25aea59e6cdaf36da4994870218529910508f
Harun Bilge <harunbilge20@gmail.com>
2026-07-03 09:54:38 +0300

    feat(part01): add student report calculations

--- part01-studentLifeCalculator/src/StudentLifeCalculator.java
+++ part01-studentLifeCalculator/src/StudentLifeCalculator.java
@@ -22,5 +22,67 @@ public class StudentLifeCalculator {
         int monthlyTransportExpense = Integer.valueOf(scan.nextLine());  
         System.out.print("Gift Money Received: ");
         int giftMoneyReceived = Integer.valueOf(scan.nextLine());  
+
+        String studentStatus = "";
+        String grade = "";
+        String remainingMoneyText = "";
+        String scholarship = "";
+        String giftTaxStatus = "";
+        double tax = 0;
+        int totalExpenses = 0;
+        int remainingMoney = 0;
+        if (age >= 18) {
+            studentStatus = "Adult Student";
+        }
+        else {
+            studentStatus = "Underage Student";
+        }
+
+        if (examPoints == 100) {
+            grade = "Incredible";
+        }
+        else if (examPoints > 90) {
+            grade = "AA";
+        }
+        else if (examPoints > 80) {
+            grade = "BB";
+        }
+        else if (examPoints > 70) {
+            grade = "CB";
+        }
+        else if (examPoints >=60) {
+            grade = "CC";
+        }
+        else {
+            grade = "Failed";
+        }
+
+        totalExpenses = monthlyRent+monthlyFoodExpense+monthlyTransportExpense;
+        remainingMoney = monthlyIncome - totalExpenses;
+
+        if (remainingMoney > 0 ) {
+            remainingMoneyText = "You saved money this month";           
+        }
+        else if (remainingMoney == 0) {
+            remainingMoneyText = "Your budget is balanced";
+        }
+        else {
+            remainingMoneyText = "You spent more than you earned";
+        }
+
+        if (examPoints >= 80 && monthlyIncome < 15000) {
+            scholarship = "Eligible for scholarship";
+        }
+        else {
+            scholarship = "Not eligible for scholarship";
+        }
+
+        if (giftMoneyReceived >= 5000) {
+            tax = giftMoneyReceived * 0.25;
+            giftTaxStatus = "Your tax is: " + tax;
+        }
+        else if (giftMoneyReceived < 5000) {
+            giftTaxStatus = "No tax";
+        }
     }
 }
