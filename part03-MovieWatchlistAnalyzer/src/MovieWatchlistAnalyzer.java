6a3bf5a5b4abd68ccadf4f0a904161e9dd8772aa
Harun Bilge <harunbilge20@gmail.com>
2026-07-06 13:46:46 +0300

    feat(part03): add movie add and movie list features

--- part03-MovieWatchlistAnalyzer/src/MovieWatchlistAnalyzer.java
+++ part03-MovieWatchlistAnalyzer/src/MovieWatchlistAnalyzer.java
@@ -1,7 +1,76 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MovieWatchlistAnalyzer{
+public class MovieWatchlistAnalyzer {
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> movies = new ArrayList<>();
+    
+        while (true) {
+            System.out.println("Command:");
+            String command = scanner.nextLine();
+
+            if (command.equals("quit")) {
+                break;
+            }
+
+            if (command.equals("add")) {
+                System.out.println("Movie name, year, rating and genre: ");
+                String movieInfo = scanner.nextLine();
+                movies.add(movieInfo);
+            }
+
+            if (command.equals("list")) {
+                printMovies(movies);
+            }
+
+            if (command.equals("search")) {
+                System.out.println("Search word:");
+                String word = scanner.nextLine();
+                searchMovies(movies, word);
+            }
+
+            if (command.equals("best")) {
+                printBestMovie(movies);
+            }
+
+            if (command.equals("average")) {
+                printAverageRating(movies);
+            }
+
+            if (command.equals("genres")) {
+                printGenres(movies);
+            }
+        }
+    }
+
+    public static void printMovies(ArrayList<String> movies) {
+        
+        for (String movie : movies ) {
+            
+            String[] parts = movie.split(",");
+            String name = parts[0];
+            int year = Integer.valueOf(parts[1]);
+            double rating = Double.valueOf(parts[2]);
+            int genre = Integer.valueOf(parts[3]);
+            System.out.println(name + " (" + year + "), rating: " + rating + ", genre: " + genre);
+        }
+    }
+
+    public static void searchMovies(ArrayList<String> movies, String word) {
+
+    }
+
+    public static void printBestMovie(ArrayList<String> movies) {
+
+    }
+
+    public static void printAverageRating(ArrayList<String> movies) {
+
+    }
+
+    public static void printGenres(ArrayList<String> movies) {
+
     }
 }
