d6173942a7b4594200cdf8166d36e7b6c80a3b2d
Harun Bilge <harunbilge20@gmail.com>
2026-07-09 15:04:26 +0300

    feat(part05): added simple date class and simple date features

--- part05-CourseEnrollmentSystem/src/SimpleDate.java
+++ part05-CourseEnrollmentSystem/src/SimpleDate.java
@@ -4,27 +4,69 @@ public class SimpleDate {
     private int year;
 
     public SimpleDate(int day, int month, int year) {
-        // TODO
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
 
     public void advance() {
-        // TODO
+        if (this.month == 12 && this.day == 30) {
+            this.day = 1;
+            this.month = 1;
+            this.year++;
+        }
+        else if (this.day == 30) {
+            this.day = 1;
+            this.month++;
+        }
+        else {
+            this.day++;
+        }
     }
 
     public void advance(int days) {
-        // TODO
+        if (this.month == 12 && this.day == 30-days) {
+            this.day = 1;
+            this.month = 1;
+            this.year++;
+        }
+        else if (this.day == 30-days) {
+            this.day = 1;
+            this.month++;
+        }
+        else {
+            this.day +=days;
+        }
     }
 
     public SimpleDate afterNumberOfDays(int days) {
-        // TODO
-    }
+        SimpleDate yeni = new SimpleDate(this.day, this.month, this.year);
+        yeni.advance(days);
+        return yeni;
+}
 
     public boolean before(SimpleDate compared) {
-        // TODO
+        if (this == compared) {
+            return false;
+        }
+        if (!(compared instanceof SimpleDate)) {
+            return false;
+        }
+        SimpleDate comparedDate = (SimpleDate) compared;
+        if (comparedDate.year > this.year) {
+            return true;
+        }
+        else if (comparedDate.month > this.month) {
+            return true;
+        }
+        else if (comparedDate.day > this.day) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public String toString() {
-        // TODO
+        return this.day + ":" + this.month + ":" + this.year;
     }
 }
