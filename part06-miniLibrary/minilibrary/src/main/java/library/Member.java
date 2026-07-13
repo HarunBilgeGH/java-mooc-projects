d4e5e54cb7be8703fa614787ce3b59c6d689ef9d
Harun Bilge <harunbilge20@gmail.com>
2026-07-13 11:32:29 +0300

    feat(part06): add member class and member test

--- part06-miniLibrary/minilibrary/src/main/java/library/Member.java
+++ part06-miniLibrary/minilibrary/src/main/java/library/Member.java
@@ -4,23 +4,34 @@ public class Member {
     private String name;
     private int memberId;
     public Member(String name, int memberId) {
-
+        this.name = name;
+        this.memberId = memberId;
     }
 
     public String getName() {
-        return "";
+        return this.name;
     }
     public int getMemberId() {
-        return -1;
+        return this.memberId;
     }
 
     @Override
     public boolean equals(Object compared) {
+    if (this == compared) {
+        return true;
+    }
+
+    if (!(compared instanceof Member)) {
         return false;
     }
 
+    Member comparedMember = (Member) compared;
+
+    return this.memberId == comparedMember.getMemberId();
+}
+
     @Override
     public String toString() {
-        return "";
+        return this.name +" (#" + this.memberId + ")";
     }
 }
--- part06-miniLibrary/minilibrary/src/test/java/library/MemberTest.java
+++ part06-miniLibrary/minilibrary/src/test/java/library/MemberTest.java
@@ -1,19 +1,30 @@
 package library;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import org.junit.Test;
 
 public class MemberTest {
 
     @Test
-    public void booksWithSameTitleAndAuthorAreEqual() {
+    public void membersWithSameIdAreEqual() {
+        Member memberFirst = new Member("Harun",1);
+        Member memberSecond = new Member("Mehmet",1);
+
+        assertEquals(memberFirst,memberSecond);
     }
 
     @Test
-    public void booksWithDifferentAuthorAreNotEqual() {
+    public void membersWithDifferentIdAreNotEqual() {
+        Member memberFirst = new Member("Harun",1);
+        Member memberSecond = new Member("Mehmet",2);
+
+        assertNotEquals(memberFirst,memberSecond);
     }
 
     @Test
-    public void bookToStringIsCorrect() {
+    public void memberToStringIsCorrect() {
+        Member memberFirst = new Member("Harun",1);
+        assertEquals("Harun (#1)", memberFirst);
     }
 }
